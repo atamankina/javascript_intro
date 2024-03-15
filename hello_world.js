d068490780d1a8f9bbfdbc9a537f4c9add7ec41d
Galina Atamankina <g.atamankina@gmail.com>
2024-03-15 12:19:09 +0100

    Adding the hello world and data type examples.

---
+++ hello_world.js
@@ -0,0 +1,23 @@
+// Das ist ein einzeiliger Kommentar
+// Gebe Hello World! in der Konsole aus.
+console.log("Hello World!");
+
+/* 
+Das ist ein
+mehrzeiliger Kommentar.
+*/
+
+//var hat Funktions-Scope oder globalen Scope
+var limitedText = "Diese Variable hat den Funktions-Scope." 
+
+//let hat Block-Scope {}
+let blockText = "Diese Variable hat einen Block-Scope."
+
+//const veraendert sich nicht
+const constVariable = "Dieser Wert veraendert sich nicht."
+
+/*
+Namenskonventionen: 
+Variablennamen sollten aussagekräftig sein 
+und die camelCase-Schreibweise verwenden.
+*/
